d86f812aaf46c60ed762634c2e25b25dede0e209
Joao-Terencio <jh.terencio@gmail.com>
2025-02-18 23:58:06 -0300

    Functions Components

---
+++ Studies/FunctionComponents.md
@@ -0,0 +1,2 @@
+# Functions Components
+Functions are objects, but web can type the params or entrire function or return
--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import "./Card.css"
 
 // * The interface provide to us all the labels for our data. With interface we can define the type of the data that is required
@@ -9,7 +9,7 @@ interface Props {
 }
 
 // * When we write this { companyName, ticker, price } is like a function that recive an argument, and we can do whatever we want to this arguments
-const Card = ({ companyName, ticker, price }: Props) => {
+const Card: React.FC<Props> = ({ companyName, ticker, price }: Props): JSX.Element => {
   return <div className="card">
     <img src="" alt="preguica" />
     <div className="details">
@@ -21,3 +21,17 @@ const Card = ({ companyName, ticker, price }: Props) => {
 }
 
 export default Card
+
+// * Another way to make this is this way
+// * The React.FC<Props> is used to declare that a component is functional and he must recive props
+// * The JSX.Element is the value type that the components will return. Here, the component return is typed as JSX.Element, which means TypeScript will ensure that you are returning something that is valid for React to render.
+// const Card: React.FC<Props> = ({ companyName, ticker, price }): JSX.Element => {
+//   return <div className="card">
+//     <img src="" alt="preguica" />
+//     <div className="details">
+//       <h2>{companyName} ({ticker})</h2>
+//       <p>${price}</p>
+//     </div> 
+//     <p className="infon">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, accusantium.</p>
+//   </div>
+// }
